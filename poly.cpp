374d8b7391af12f4f872c7fe82d29f02dca36108
DavDori <davide.dorigoni@studenti.unitn.it>
2020-02-27 20:34:19 +0100

    add subtraction in the example program

--- poly.cpp
+++ poly.cpp
@@ -10,22 +10,20 @@ int main()
   vector<float> d1 = {1,1};
 
   vector<float> n2 = {1};
-  vector<float> d2 = {1};
 
   Polynomial poly1(n1,d1);
-  Polynomial poly2(n2,d2);
+  Polynomial poly2(n2);
+
   cout << "polynomal 1 : " << poly1.print() << endl;
   cout << "polynomal 2 : " << poly2.print() << endl;
+
   Polynomial polyMult = poly1 * poly2;
-  cout << "1 * 2 : " << polyMult.print() << endl;
+  cout << "polynomal 1 * polynomal 2 : " << polyMult.print() << endl;
 
-  Polynomial polyAdd = (poly1 + poly2);
-  cout << "1 + 2 : " << polyAdd.print() << endl;
+  Polynomial polyAdd = poly1 + poly2;
+  cout << "polynomal 1 + polynomal 2 : " << polyAdd.print() << endl;
 
-
-
-
-
-
+  Polynomial polySub = poly1 - poly2;
+  cout << "polynomal 1 - polynomal 2 : " << polySub.print() << endl;
   return 0;
 }
